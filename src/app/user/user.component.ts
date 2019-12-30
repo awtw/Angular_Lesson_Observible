ead589558e9ae5867e56762594aa40bf615980fd
awtw <alec.wang.tpe@gmail.com>
2019-12-31 00:00:03 +0800

    pipe map filter subject next

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,28 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { interval, Subscription } from 'rxjs';
+import { UserService } from '../user.service';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+	selector: 'app-user',
+	templateUrl: './user.component.html',
+	styleUrls: [ './user.component.css' ]
 })
 export class UserComponent implements OnInit, OnDestroy {
+	private fristObsSubscription: Subscription;
+	constructor(private route: ActivatedRoute, private userservice: UserService) {}
 
+	ngOnInit() {
+		this.fristObsSubscription = interval(1000).subscribe((count) => {
+			console.log(count);
+		});
+	}
 
-  private fristObsSubscription: Subscription;
-  constructor(private route: ActivatedRoute) {
-  }
+	ngOnDestroy() {
+		this.fristObsSubscription.unsubscribe();
+	}
 
-  ngOnInit() {
-    this.fristObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
-    })
-  }
-
-  ngOnDestroy(){
-    this.fristObsSubscription.unsubscribe();
-  }
-
+	onActivate() {
+		this.userservice.activatedEmitter.next(true);
+	}
 }
