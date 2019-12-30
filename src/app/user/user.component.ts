a139c1ea335f8ff2e7153e5071b28cd169b00e7e
alec wang <alex0123321@gmail.com>
2019-12-30 15:46:31 +0800

    using own observible method, but have to deal with unsubscrible

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  id: number;
+export class UserComponent implements OnInit, OnDestroy {
 
+
+  private fristObsSubscription: Subscription;
   constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
-      this.id = +params.id;
-    });
+    this.fristObsSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    })
   }
+
+  ngOnDestroy(){
+    this.fristObsSubscription.unsubscribe();
+  }
+
 }
